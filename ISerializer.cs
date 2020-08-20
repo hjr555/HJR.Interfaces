3b9c5dbaea6ad2b3267a10820a6d124081cfe797
Hywel Rees <hywel.rees@gocompare.com>
2020-08-20 11:48:40 +0100

    Update to 2.1, serialiser and oembed

---
+++ ISerializer.cs
@@ -0,0 +1,10 @@
+﻿using System.Threading.Tasks;
+
+namespace HJR.Interfaces
+{
+    public interface ISerializer<T>
+    {
+        Task<string> SerializeAsync(T obj);
+        Task<T> DeserializeAsync(string json);
+    }
+}
---
+++ Ioembed.cs
@@ -0,0 +1,9 @@
+﻿using System.Threading.Tasks;
+
+namespace HJR.Interfaces
+{
+    public interface IoEmbedService<T>
+    {
+        public Task<T> GetAsync(int id);
+    }
+}
